5d8abebeba5ddd517f355a114c742ddf7837c9f6
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-03-21 17:26:50 +0000

    changes in binary crate to reflect the new subcrates

--- src/game/src/main.rs
+++ src/game/src/main.rs
@@ -1,3 +1,11 @@
+use bevy::prelude::*;
+use level::level_map::MapPlugin;
+use state::{GameStatePlugin, GlobalState};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(MapPlugin {})
+        .add_plugins(GameStatePlugin {})
+        .run();
 }
