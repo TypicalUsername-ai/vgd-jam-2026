f98cbe376b0f41770d676b213b343194674c89d0
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-03-21 20:07:56 +0000

    bin: resturcture main menu to separate file

--- src/game/src/main.rs
+++ src/game/src/main.rs
@@ -1,6 +1,8 @@
+mod menu;
 use bevy::prelude::*;
 use level::level_map::MapPlugin;
-use state::{GameStatePlugin, GlobalState};
+use menu::MainMenuPlugin;
+use state::GameStatePlugin;
 use tracing::info;
 
 fn main() {
@@ -10,43 +12,4 @@ fn main() {
         .add_plugins(GameStatePlugin {})
         .add_plugins(MainMenuPlugin {})
         .run();
-}
-
-struct MainMenuPlugin {}
-
-impl Plugin for MainMenuPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GlobalState::StartMenu), draw_main_menu);
-    }
-}
-
-fn draw_main_menu(mut commands: Commands) {
-    info!("spawning main menu text");
-    commands.spawn((Camera2d, IsDefaultUiCamera));
-    commands.spawn((
-        Node {
-            width: percent(100),
-            height: percent(100),
-            display: Display::Flex,
-            justify_content: JustifyContent::SpaceAround,
-            align_items: AlignItems::Center,
-            flex_direction: FlexDirection::Column,
-            justify_items: JustifyItems::Center,
-            ..default()
-        },
-        children![Text::new("Main Menu"), Text::new("Level selection")],
-    ));
-    /*
-    commands.spawn((
-        Text::new("Main Menu"),
-        TextLayout::new_with_justify(Justify::Center),
-        TextFont {
-            font_size: 24.0,
-            font: default(),
-            ..default()
-        },
-        TextColor(Color::WHITE),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-    ));
-    */
 }
---
+++ src/game/src/menu.rs
@@ -0,0 +1,41 @@
+use bevy::prelude::*;
+use state::GlobalState;
+
+pub(crate) struct MainMenuPlugin {}
+
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GlobalState::StartMenu), draw_main_menu);
+    }
+}
+
+fn draw_main_menu(mut commands: Commands) {
+    info!("spawning main menu text");
+    commands.spawn((Camera2d, IsDefaultUiCamera));
+    commands.spawn((
+        Node {
+            width: percent(100),
+            height: percent(100),
+            display: Display::Flex,
+            justify_content: JustifyContent::SpaceAround,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            justify_items: JustifyItems::Center,
+            ..default()
+        },
+        children![Text::new("Main Menu"), Text::new("Level selection")],
+    ));
+    /*
+    commands.spawn((
+        Text::new("Main Menu"),
+        TextLayout::new_with_justify(Justify::Center),
+        TextFont {
+            font_size: 24.0,
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
+    */
+}
