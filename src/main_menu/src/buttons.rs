023f4cf9a0eaea45f009dca1eecd736eaa3960e6
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-03-23 13:22:40 +0000

    main_menu: add buttons for state changes and wip state change to level select

--- src/main_menu/src/buttons.rs
+++ src/main_menu/src/buttons.rs
@@ -1,13 +1,42 @@
 use crate::MenuButtons;
-use bevy::{prelude::*, ui::Pressed};
+use bevy::prelude::*;
+use state::GlobalState;
 use tracing::info;
 
-pub(crate) fn react_buttons(query: Query<(&MenuButtons, &Interaction), Changed<Interaction>>) {
+pub(crate) fn react_buttons(
+    query: Query<(&MenuButtons, &Interaction), Changed<Interaction>>,
+    mut next_global: ResMut<NextState<GlobalState>>,
+) {
     for (button, interaction) in query {
-        match *interaction {
-            Interaction::Pressed => info!("{:?} pressed", button),
-            Interaction::Hovered => info!("Hover"),
-            Interaction::None => info!("None"),
+        match *button {
+            MenuButtons::Levels => handle_levels(interaction, &mut next_global),
+            MenuButtons::Options => handle_options(interaction),
+            MenuButtons::Quit => handle_quit(interaction),
         }
+    }
+}
+
+fn handle_levels(action: &Interaction, next_global: &mut ResMut<NextState<GlobalState>>) {
+    match action {
+        Interaction::Pressed => {
+            warn!("Moving to level select");
+            next_global.set(GlobalState::LevelSelect);
+        }
+        Interaction::Hovered => (),
+        Interaction::None => (),
+    }
+}
+fn handle_quit(action: &Interaction) {
+    match action {
+        Interaction::Pressed => todo!(),
+        Interaction::Hovered => (),
+        Interaction::None => (),
+    }
+}
+fn handle_options(action: &Interaction) {
+    match action {
+        Interaction::Pressed => todo!(),
+        Interaction::Hovered => (),
+        Interaction::None => (),
     }
 }
