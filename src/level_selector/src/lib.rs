82961f5bdaf11a216fc9a35cfcb3cd13b963dfc4
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-03-25 08:29:02 +0000

    starter level select ui

--- src/level_selector/src/lib.rs
+++ src/level_selector/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use bevy::prelude::*;
+use state::GlobalState;
+mod ui;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct MainMenuPlugin {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GlobalState::LevelSelect), ui::draw_level_select);
     }
 }
---
+++ src/level_selector/src/ui.rs
@@ -0,0 +1,84 @@
+use bevy::{color::palettes::tailwind, prelude::*};
+use state::GlobalState;
+
+pub(crate) fn draw_level_select(mut commands: Commands) {
+    info!("spawning main menu text");
+    commands.spawn((
+        Node {
+            width: percent(100),
+            height: percent(100),
+            display: Display::Flex,
+            justify_content: JustifyContent::SpaceAround,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            justify_items: JustifyItems::Center,
+            ..default()
+        },
+        children![
+            level_select_title("Game main menu"),
+            available_levels_bundle()
+        ],
+    ));
+}
+
+fn available_levels_bundle() -> impl Bundle {
+    (
+        Node {
+            width: percent(100),
+            height: percent(80),
+            display: Display::Grid,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::SpaceEvenly,
+            ..default()
+        },
+        // BackgroundColor(tailwind::BLUE_300.into()),
+        children![make_button("Demo level", Button)],
+    )
+}
+
+fn level_select_title(title: impl Into<String>) -> impl Bundle {
+    (
+        Node {
+            width: percent(100),
+            height: percent(20),
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::SpaceEvenly,
+            justify_items: JustifyItems::Center,
+            ..default()
+        },
+        children![(
+            Text::new(title),
+            TextFont {
+                font_size: 44.0,
+                font: default(),
+                ..default()
+            },
+            TextColor(Color::WHITE),
+        )],
+    )
+}
+
+fn make_button(text: impl Into<String>, component: impl Component) -> impl Bundle {
+    (
+        Node {
+            border: UiRect::all(px(5.0)),
+            min_height: px(40),
+            padding: UiRect::all(px(10)),
+            border_radius: BorderRadius::all(px(10)),
+            ..default()
+        },
+        children![(
+            Text::new(text),
+            TextFont {
+                font_size: 22.0,
+                font: default(),
+                ..default()
+            },
+            TextColor(Color::BLACK),
+        )],
+        BackgroundColor(Color::WHITE),
+        component,
+    )
+}
