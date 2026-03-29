3f76236c7305d333e8f53a812a95f12dc5e4eb5f
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-03-29 17:00:23 +0000

    main: tweak asset loader

--- src/game/src/main.rs
+++ src/game/src/main.rs
@@ -10,7 +10,14 @@ fn main() {
     let config = std::path::Path::new("../assets/level-config.ron");
 
     App::new()
-        .add_plugins(DefaultPlugins.set(window::default_fulscreen_plugin()))
+        .add_plugins(
+            DefaultPlugins
+                .set(window::default_fulscreen_plugin())
+                .set(AssetPlugin {
+                    file_path: "../assets".into(),
+                    ..default()
+                }),
+        )
         .add_plugins(GameStatePlugin {})
         .add_plugins(MainMenuPlugin {})
         .add_plugins(LevelSelectPlugin::from(config))
