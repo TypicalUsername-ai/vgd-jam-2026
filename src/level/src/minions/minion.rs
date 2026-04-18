c5ef62a2f0b2b46351e4f1b58921c8aac98b668a
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-04-18 15:47:27 +0000

    minions: add minion struct

---
+++ src/level/src/minions/minion.rs
@@ -0,0 +1,20 @@
+//use super::Action;
+use bevy::prelude::*;
+use std::collections::VecDeque;
+
+#[derive(Component)]
+pub struct Minion {
+    kind: MinionKind,
+    state: MinionState,
+    health: usize,
+    speed: f32,
+    pub distance_traveled: f32,
+    // to animation state sprite_range: (usize, usize),
+    // same ^^ sprite_index: usize,
+}
+
+pub enum MinionKind {
+    Chicken,
+}
+
+pub enum MinionState {}
--- src/level/src/minions/mod.rs
+++ src/level/src/minions/mod.rs
@@ -1,7 +1,9 @@
 mod animation;
 mod damage;
+mod minion;
 mod movement;
 
 pub(crate) use animation::animate_minions;
 pub(crate) use damage::handle_damage;
+pub(crate) use minion::Minion;
 pub(crate) use movement::move_minions;
